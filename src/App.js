3c9a701e0e8422c92472b54c3f9d2209394f1b1b
Kevin Ndung'u <kevgathuku@gmail.com>
2017-08-14 19:17:34 +0300

    Use raw Konva without React components

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import Konva from 'konva';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: 'green'
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  renderKonva(container) {
+    let stage = new Konva.Stage({
+      container: container,
+      width: 100,
+      height: 100
+    });
+
+    let layer = new Konva.Layer();
+    let rect = new Konva.Rect({
+      x: 50,
+      y: 50,
+      width: 100,
+      height: 50,
+      fill: this.state.color
+    });
+
+    layer.add(rect);
+    stage.add(layer);
+  }
+
+  handleClick() {
+    this.setState({
+      color: Konva.Util.getRandomColor()
+    });
+  }
+  render() {
+    return (
+      <div ref={ref => this.renderKonva(ref)} onClick={this.handleClick} />
+    );
+  }
+}
+
+export default App;
